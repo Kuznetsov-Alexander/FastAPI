e2f2fb14037779b8e1256478b7f77a589946d744
Alexander <sashakuznetsov64@gmail.com>
2025-04-12 18:40:40 +0300

    Merge

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query
+from typing import Optional
+from datetime import date
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, date_from, date_to):
-    return item_id, date_from, date_to
+@app.get("/hotels/")
+def read_item(item_id: int,
+              location: str,
+              date_from: date,
+              date_to: date,
+              has_spa: Optional[bool] = None,
+              stars: Optional[int] = Query(None, ge = 1, le = 5),
+              ):
+    return date_from, date_to
 
 
 
