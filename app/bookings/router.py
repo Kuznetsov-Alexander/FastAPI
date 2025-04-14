23eded480617af726229f92902f61457a359595b
Alexander <sashakuznetsov64@gmail.com>
2025-04-14 21:03:00 +0300

    add schemas to validate data

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -3,10 +3,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.bookings.dao import BookingDAO
 from app.database import get_async_session
+from app.bookings.schemas import SBooking
+
 router = APIRouter(prefix="/bookings",
                    tags=["Bookings"])
 
 
 @router.get("")
-async def get_bookings(session: AsyncSession = Depends(get_async_session)):
-    return await BookingDAO.find_one_or_none(session, room_id = 3)
+async def get_bookings(session: AsyncSession = Depends(get_async_session)) -> list[SBooking]:
+    return await BookingDAO.find_all(session)
