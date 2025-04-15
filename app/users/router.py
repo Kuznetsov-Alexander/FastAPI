73e24ca1c8e62d86afc6be218623aabc0ac0b9ee
Alexander <sashakuznetsov64@gmail.com>
2025-04-15 19:13:04 +0300

    create endpoint for login

--- app/users/router.py
+++ app/users/router.py
@@ -1,13 +1,13 @@
 from http.client import HTTPException
 
-from fastapi import APIRouter, Depends
+from alembic.util import status
+from fastapi import APIRouter, Depends, status, Response
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.testing.pickleable import User
 
 from app.database import get_async_session
-from app.users.auth import get_password_hash
+from app.users.auth import get_password_hash, verify_password, authenticate_user, create_access_token
 from app.users.dao import UsersDAO
-from app.users.schemas import SUserRegister
+from app.users.schemas import SUserAuth
 
 router = APIRouter(
     prefix="/auth",
@@ -15,9 +15,18 @@ router = APIRouter(
 )
 
 @router.post("/register")
-async def register_user(user_data: SUserRegister, session: AsyncSession = Depends(get_async_session)):
+async def register_user(user_data: SUserAuth, session: AsyncSession = Depends(get_async_session)):
     existing_user = await UsersDAO.find_one_or_none(session, email=user_data.email)
     if existing_user:
         raise HTTPException(status_code=500)
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(session, email=user_data.email, hashed_password=hashed_password)
+
+@router.post("/login")
+async def login_user(response: Response ,user_data: SUserAuth, session: AsyncSession = Depends(get_async_session)):
+    user = await authenticate_user(user_data.email, user_data.password, session)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token({"sub": user.id})
+    response.set_cookie("booking_access_token", access_token, httponly=True)
+    return access_token
