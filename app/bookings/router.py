8206f228d174c3fc952f42883410656ea40f5f8c
Alexander <sashakuznetsov64@gmail.com>
2025-04-15 21:43:33 +0300

    give correct results to user in accordance with the user_id

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -4,11 +4,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.bookings.dao import BookingDAO
 from app.database import get_async_session
 from app.bookings.schemas import SBooking
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 
 router = APIRouter(prefix="/bookings",
                    tags=["Bookings"])
 
 
 @router.get("")
-async def get_bookings(session: AsyncSession = Depends(get_async_session)) -> list[SBooking]:
-    return await BookingDAO.find_all(session)
+async def get_bookings(
+    user: Users = Depends(get_current_user),
+    session: AsyncSession = Depends(get_async_session)
+) -> list[SBooking]:
+    return await BookingDAO.find_all(session, user_id=user.id)
--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -1,14 +1,37 @@
-from fastapi import Request, HTTPException
+from datetime import datetime
+
+from fastapi import Request, HTTPException, Depends, status
+from jose import jwt, JWTError
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from sqlalchemy.sql.functions import current_user
+
+from app.config import settings
+from app.database import get_async_session
+from app.users.dao import UsersDAO
 
 
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(status_code=401)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     return token
 
-def get_current_user(token):
-    
-    return current_user
+async def get_current_user(token:str = Depends(get_token), session: AsyncSession = Depends(get_async_session)):
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    expire: str = payload.get("exp")
+    if not expire or datetime.utcfromtimestamp(int(expire)) < datetime.utcnow():
+        raise HTTPException(status_code=401, detail="Token expired")
+    user_id: str = payload.get("sub")
+    if (not user_id):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    user = await UsersDAO.find_by_id(session, int(user_id))
+    if (not user):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    return user
